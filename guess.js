8cfaae11d48abda6a62e51355f50738a50a34955
David <indiedave@gmail.com>
2020-06-20 22:55:55 -0400

    changed from recursive to a do while loop

--- guess.js
+++ guess.js
@@ -7,27 +7,46 @@
 var rando = Math.floor(Math.random() * 100) + 1;
 var guessTotal = 0;
 
-var guess = prompt("choose a whole number between 1 and 100");
+// function compare(rando, guess) {
+//   if (rando == guess) {
+//     guessTotal++;
+//     alert("You got it in " + guessTotal + " guesses.");
+//   } else if (rando > guess) {
+//     guessTotal++;
+//     alert("Guess higher!");
+//     guess = prompt("choose a whole number between 1 and 100");
+//     compare(rando, guess);
+//   } else if (guess > rando) {
+//     guessTotal++;
+//     alert("Guess lower!");
+//     guess = prompt("choose a whole number between 1 and 100");
+//     compare(rando, guess);
+//   } else {
+//     alert("You have to pick a whole number between 1-100!");
+//     guess = prompt("choose a whole number between 1 and 100");
+//     compare(rando, guess);
+//   }
+// }
 
-function compare(rando, guess) {
-  if (rando == guess) {
-    guessTotal++;
-    alert("You got it in " + guessTotal + " guesses.");
-  } else if (rando > guess) {
-    guessTotal++;
-    alert("Guess higher!");
-    guess = prompt("choose a whole number between 1 and 100");
-    compare(rando, guess);
-  } else if (guess > rando) {
-    guessTotal++;
-    alert("Guess lower!");
-    guess = prompt("choose a whole number between 1 and 100");
-    compare(rando, guess);
+// compare(rando, guess);
+//var guess = 0;
+
+//do while loop runs at least once.  put the validation in another do while loop to start this.  had to have
+//the last else statement to not return a lower so ended up moving the alert to the last else.  cant have a condition on the else
+// dont have to have a straight else to finish.  you can finish with an else if and jeff said you can just have an if as well
+do {
+  do {
+    var guess = prompt("choose a whole number between 1 and 100");
+    //this ! in front of this statement means the inverse of it.  we break the loop only if guess satisies both of these b/c the statement is to keep looping
+    //if it does not equall this.
+  } while (!(guess >= 1 && guess <= 100));
+  guessTotal++;
+
+  if (rando > guess) {
+    alert("Guess higher");
+  } else if (rando < guess) {
+    alert("Guess lower");
   } else {
-    alert("You have to pick a whole number between 1-100!");
-    guess = prompt("choose a whole number between 1 and 100");
-    compare(rando, guess);
+    alert("you got it in " + guessTotal + " guesses");
   }
-}
-
-compare(rando, guess);
+} while (rando != guess);
